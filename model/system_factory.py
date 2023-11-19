d97082451b36c48fbcd19fe55303e9c07d92255b
jiaxilyu <jlyu3@ualberta.ca>
2023-11-19 14:16:14 -0500

    optimize model structure and adding trainer feature

--- model/system_factory.py
+++ model/system_factory.py
@@ -1,15 +1,15 @@
-from classifier import Classifier
-from extractor import GPT2Extractor
+from model.classifier import Classifier
+from model.extractor import GPT2Extractor
 from transformers import AutoTokenizer
-from emofilmsystem import EmoFilmSystem
+from model.emofilmsystem import EmoFilmSystem
 import torch.nn as nn
 
 
 class SystemFactory:
     @staticmethod
     def ExtractorFactory(pre_train_model_name, train_extractor):
-        if pre_train_model_name == "gpt2":
-            return GPT2Extractor(train_extractor)
+        if pre_train_model_name in ["gpt2", "gpt2-medium"]:
+            return GPT2Extractor(pre_train_model_name, train_extractor)
         else:
             raise Exception(f'no available pre_train_model {pre_train_model_name}')
     
@@ -20,10 +20,8 @@ class SystemFactory:
     @staticmethod
     def produce_system(extractor_config={"pre_train_model_name":"gpt2", "train_extractor":True}, classifier_config={"number_classes":3, "hidden_units":[64, 32]}, Lossfn=nn.CrossEntropyLoss):
         tokenizer = AutoTokenizer.from_pretrained(extractor_config["pre_train_model_name"])
+        tokenizer.pad_token = tokenizer.eos_token
         feature_extractor = SystemFactory.ExtractorFactory(**extractor_config)
         classifier_config["number_features"] = feature_extractor.get_output_shape()[-1]
         classifier = SystemFactory.ClassifierFactory(**classifier_config)
         return EmoFilmSystem(tokenizer=tokenizer, feature_extractor=feature_extractor, classifier=classifier, Lossfn=Lossfn)
-
-emofilmsystem = SystemFactory.produce_system()
-print(emofilmsystem.inference("test model"))
